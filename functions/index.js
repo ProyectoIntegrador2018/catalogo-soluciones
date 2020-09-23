74034f8231a36e3158d69243b163d413de6646ce
mauriciogm97 <mauriciogm97@hotmail.com>
2020-09-23 11:02:05 -0500

    Set up express server

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
