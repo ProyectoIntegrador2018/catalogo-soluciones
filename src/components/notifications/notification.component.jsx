db63ee83cb6c9061eca84f7505af292e6ab67df2
mauriciogm97 <mauriciogm97@hotmail.com>
2020-10-19 16:37:48 -0500

    Only one error mssg

--- src/components/notifications/notification.component.jsx
+++ src/components/notifications/notification.component.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 
-import './form-error.styles.scss';
+import './notification.styles.scss';
 
 const Alert = (props) => {
   return <MuiAlert elevation={6} variant='filled' {...props} />;
 };
 
-const FormError = ({ errorMssg, onClose, ...otherProps }) => (
+export const FormError = ({ errorMssg, onClose, ...otherProps }) => (
   <Snackbar
     autoHideDuration={5000}
     anchorOrigin={{
@@ -24,4 +24,18 @@ const FormError = ({ errorMssg, onClose, ...otherProps }) => (
   </Snackbar >
 );
 
-export default FormError;
+export const Message = ({ mssg, onClose, ...otherProps }) => (
+  <Snackbar
+    autoHideDuration={5000}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'left',
+    }}
+    open={mssg ? true : false}
+    onClose={onClose}
+  >
+    <Alert severity='info' onClose={onClose}>
+      {mssg}
+    </Alert>
+  </Snackbar >
+);
