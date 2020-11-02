a29eaa9ab5cbb7c8d12a10c41239dd81e66d5833
Enrique Villa <enrique.villa97@hotmail.com>
2020-11-02 17:57:17 -0600

    Recreate catalog using Material Table

--- src/components/catalog-list/catalog-list.component.jsx
+++ src/components/catalog-list/catalog-list.component.jsx
@@ -1,28 +1,125 @@
 import React from 'react';
+import { forwardRef } from 'react';
 
-import CatalogItem from '../catalog-item/catalog-item.component';
+import MaterialTable from 'material-table';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
 import './catalog-list.styles.scss';
 
-const CatalogList = ({ data }) => (
-  <div className='catalog-list'>
-    <div className='catalog-header'>
-      <div className='header-block'>
-        <span>Organizacion</span>
-      </div>
-      <div className='header-block'>
-        <span>Solucion</span>
-      </div>
-      <div className='header-block-small'>
-        <span>Precio</span>
-      </div>
-    </div>
-    {data
-      ? data.map((value, index) => {
-          return <CatalogItem key={index} data={value} />;
-        })
-      : null}
-  </div>
-);
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight {...props} ref={ref} />
+  )),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
+
+const CatalogList = ({ data }) => {
+  if (data) {
+    data.forEach((i) => {
+      i.imageUrl = <img src={i.imageUrl} width='50px' alt='logo' />;
+    });
+  }
+  return (
+    data && (
+      <MaterialTable
+        title=''
+        icons={tableIcons}
+        columns={[
+          {
+            title: '',
+            field: 'imageUrl',
+            cellStyle: {
+              width: '5%',
+            },
+            headerStyle: {
+              width: '5%',
+            },
+          },
+          { title: 'Organización', field: 'organization' },
+          { title: 'Solución', field: 'solutionName' },
+          { title: 'Categoría', field: 'category' },
+          {
+            title: 'Precio',
+            field: 'price',
+            type: 'currency',
+            filterCellStyle: {
+              textAlign: 'right',
+            },
+          },
+        ]}
+        data={data}
+        detailPanel={(rowData) => {
+          return (
+            <>
+              <h3 className='description-title'>Descripción del servicio</h3>
+              <p className='description-body separate'>
+                {rowData.descriptionPitch}
+              </p>
+              <h3 className='description-title'>Casos de éxito del servicio</h3>
+              <p className='description-body'>{rowData.descriptionSuccess}</p>
+            </>
+          );
+        }}
+        localization={{
+          toolbar: {
+            searchTooltip: 'Buscar',
+            searchPlaceholder: 'Buscar',
+          },
+          body: {
+            emptyDataSourceMessage: 'No hay información que mostrar',
+            filterRow: {
+              filterTooltip: 'Filtrar',
+            },
+          },
+          pagination: {
+            labelRowsSelect: 'filas',
+            labelDisplayedRows: '{from}-{to} de {count}',
+            firstTooltip: 'Primera página',
+            previousTooltip: 'Página anterior',
+            nextTooltip: 'Siguiente página',
+            lastTooltip: 'Última página',
+          },
+        }}
+        options={{
+          headerStyle: {
+            fontWeight: 'bolder',
+          },
+        }}
+      />
+    )
+  );
+};
 
 export default CatalogList;
