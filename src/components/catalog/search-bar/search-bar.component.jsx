73f091da6c5587bc26695586012c72f8f6de503f
mauriciogm97 <mauriciogm97@hotmail.com>
2020-11-15 22:38:29 -0600

    Create search bar for catalog

---
+++ src/components/catalog/search-bar/search-bar.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { IconButton, InputBase, Paper } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+import './search-bar.styles.scss';
+
+const SearchBar = ({ setSearchQuery }) => {
+  const [state, setState] = React.useState({
+    searchQuery: '',
+  });
+
+  const handleChange = (event) => {
+    setState({...state, searchQuery: event.target.value});
+  }
+
+  return (
+    <Paper className='search-box' variant='outlined'>
+      <InputBase 
+        className='text-input' 
+        placeholder='Búsqueda' 
+        type='text'
+        value={state.searchQuery}
+        onChange={handleChange}
+      />
+      <IconButton 
+        type="submit" 
+        onClick={() => setSearchQuery(state.searchQuery)}
+      >
+        <SearchIcon className='search-icon' />
+      </IconButton>
+    </Paper>
+  );
+}
+
+export default SearchBar;
