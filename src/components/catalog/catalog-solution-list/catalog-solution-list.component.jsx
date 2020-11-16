365e6cd5623030e3b98f42e4d51217468c5f6b99
mauriciogm97 <mauriciogm97@hotmail.com>
2020-11-16 16:23:28 -0600

    Show inquire button when no results on catalog

--- src/components/catalog/catalog-solution-list/catalog-solution-list.component.jsx
+++ src/components/catalog/catalog-solution-list/catalog-solution-list.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Pagination } from '@material-ui/lab';
 import CatalogSolutionItem from '../catalog-solution-item/catalog-solution-item.component';
+import CButton from '../../elements/c-button/c-button.component';
 
 import './catalog-solution-list.styles.scss';
 
 const CatalogSolutionList = ({ solutions }) => {
+  let history = useHistory();
+
   const [state, setState] = React.useState({
     page: 1,
   });
@@ -37,10 +41,19 @@ const CatalogSolutionList = ({ solutions }) => {
               ))}
           </span>
         :  
-          <center>
-            <h3>No hay soluciones para mostrar.</h3>
-            <h4>Intenta eliminar algunos filtros.</h4>
-          </center>
+        <div><center>
+          <h3>No hay soluciones para mostrar.</h3>
+          <h4>Intenta eliminar algunos filtros.</h4>
+          <br></br>
+          <p>
+            ¿No encuentras lo que buscabas? <br></br>
+            <CButton
+              text='Envíanos una consulta personalizada'
+              color='orange'
+              onClick={() => history.push('custom-inquiry')}
+            />
+          </p>
+        </center></div>
         }
       </div>
       
