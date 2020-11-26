3e766c3986d565003703c0c92650fa1d466d2fc9
Enrique Villa <enrique.villa97@hotmail.com>
2020-11-25 21:39:31 -0600

    Add CSOFT social media to the footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import Twitter from '@material-ui/icons/Twitter';
+import LinkedIn from '@material-ui/icons/LinkedIn';
 
 import './footer.styles.scss';
+import { YouTube } from '@material-ui/icons';
 
 const Footer = () => (
   <div className='footer'>
     <div className='footer-content'>
       <h4>Consejo de Software de Nuevo León AC</h4>
+      <div>
+        <a href='https://www.facebook.com/csoftmty/' className='social-media'>
+          <FacebookIcon color='primary' />
+        </a>
+        <a href='https://twitter.com/Csoftmty' className='social-media'>
+          <Twitter color='primary' />
+        </a>
+        <a
+          href='https://www.linkedin.com/company/nuevo-leon-software-council/about/'
+          className='social-media'
+        >
+          <LinkedIn color='primary' />
+        </a>
+        <a
+          href='https://www.youtube.com/channel/UCl6uQPgQvJ_0gNXdeCHK3Gg'
+          className='social-media'
+        >
+          <YouTube color='primary' />
+        </a>
+      </div>
       <p>Copyright @2020</p>
       <p>Padre Mier #1251 Cuarto piso, Zona Centro. Monterrey, NL.</p>
       <a
